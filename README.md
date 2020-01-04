33fb5094fce0db91949bfe93c850b914ace74ee6
kidneyweakx <kidneyweakx@gmail.com>
2020-01-05 02:35:38 +0800

    simulation

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # BAR_group5
+by: [中山管院: 商務數據分析](https://bap.cm.nsysu.edu.tw/)
+---
+
 ### 組員
 + 企管系 詹孟凌
 + 資管系 陳聖勳
@@ -7,4 +10,9 @@
 + 公事所 陳昭穎
 + 企管碩 邱聖凱
 
+---
+
 [期中競賽html](https://2019bar.github.io/BAR_group5/midterm/G5_midterm.html)
+
+---
+[期末競賽]()
---
+++ final/Simold.r
@@ -0,0 +1,73 @@
+library(manipulate)
+manipulate({
+  # 用curve指令函數設定m, b, a
+  curve(m*plogis((10/a)*(x-b)), 0, 30, lwd=2, ylim=c(0, 0.25),
+        main = TeX('$m \\cdot Logis(10(x - b)/a)$'), ylab="f(x)")
+  abline(h=seq(0,0.2,0.05),v=seq(0,30,5),col='lightgrey',lty=2)
+},
+m = slider(0.05, 0.35,  0.20, step=0.01),
+a = slider(  10,   40,    25, step=1),
+b = slider(   4,   40,    20, step=1)
+) 
+# manipulate({##########################
+#   X = seq(0,250,5)
+#   data.frame(
+#     Group=paste0('grp',i), Para=X,
+#     Gain=DP(X,m,b,a)
+#   ) %>% data.frame %>%
+#   ggplot(aes(x=Para, y=Gain, col=Group)) +
+#   geom_line(size=1.5,alpha=0.5) + theme_bw() +
+#   ggtitle("Prob. Function: f(x|m,b,a)")
+#   },#################################
+#   i=slider(1,5,1,step=1),
+#   m=slider(0.05,0.25, 0.2,step=0.01),
+#   b=slider(10,150, 20,step=5),
+#   a=slider(0,200, 30,step=5)
+# )
+
+manipulate({####################################
+  do.call(rbind, lapply(seq(5,50,0.5), function(x){
+    dp = DP(x,m,b,a)
+    B %>% mutate(
+      dp = ifelse(Buy+dp>1, 1-Buy, dp),
+      eR = dp*Rev - x
+    ) %>%
+      group_by(grp) %>% summarise(
+        Cost = x,
+        Group.Sz = n(),
+        eR.ALL = sum(eR>0),
+        eR.SEL = sum(eR[eR>0]),
+      ) } ) ) %>% 
+    ggplot(aes(x=Cost, y=eR.SEL, col=factor(grp))) +
+    geom_line(size=1.2) +
+    ggtitle("Cost Effeciency per Segment ")
+},######################################
+m = slider(0.05, 0.25,  0.20, step=0.01),
+a = slider(  10,   30,    25, step=1),
+b = slider(  10,   50,   20, step=5)
+) 
+
+# manipulate({##########################
+#   X = seq(10, 250, 5) 
+#   df = 
+#     sapply(X, function(x) {
+#       dp = DP(x,m,b,a)
+#       dp = ifelse(B$Buy[B$grp==i]+dp>1, 1-B$Buy[B$grp==i], dp)
+#       eR = dp*B$Rev[B$grp==i] - x # 改成回購的機率
+#       c(i=i, x=x, eR.ALL=sum(eR), N=sum(eR>0), eR.SEL=sum(eR[eR > 0]) )
+#     ) %>% t %>% data.frame
+#   df %>% gather('key','value',-i,-x) %>% 
+#     mutate(Instrument = paste0('I',i)) %>% 
+#     ggplot(aes(x=x, y=value, col=Instrument)) + 
+#     geom_hline(yintercept=0, linetype='dashed', col='blue') +
+#     geom_line(size=1.5,alpha=0.5) + 
+#     xlab('工具選項(成本)') + ylab('預期報償') + 
+#     ggtitle('行銷工具優化','假設行銷工具的效果是其成本的函數') +
+#     facet_wrap(~key,ncol=1,scales='free_y') + theme_bw()
+#   })) 
+#   },#################################
+#   i=slider(1,5,1,step=1),
+#   m=slider(0.05,0.25, 0.2,step=0.01),
+#   b=slider(10,150, 20,step=5),
+#   a=slider(0,200, 30,step=5)
+# )
